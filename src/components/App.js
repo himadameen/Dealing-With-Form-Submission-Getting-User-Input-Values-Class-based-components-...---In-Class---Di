5835b41caf355813ed09d3ef4ba6a59d358ba47e
Himmad Ameen <93907951+himadameen@users.noreply.github.com>
2022-06-04 21:33:00 +0530

    my changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,66 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/App.css";
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date().toLocaleTimeString(),
-      date: new Date().toLocaleDateString()
-    };
-  }
 
-  runningTime() {
-    this.setState({
-      time: new Date().toLocaleTimeString()
-    });
-  }
+const App = () => {
+  let date = new Date().toLocaleDateString();
+  let time = new Date().toLocaleTimeString();
+  const [tyme, update] = useState(time);
 
-  componentDidMount() {
-    this.timer = setInterval(() => this.runningTime(), 1000);
-  }
+  const runningTime = () => {
+    time = new Date().toLocaleTimeString();
+    update(time);
+  };
+  setInterval(runningTime, 1000);
 
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
+  useEffect(() => {
+    clearInterval(runningTime);
+  });
 
-  render() {
-    return (
-      <>
-        <div className="date-time">
-          {this.state.date + ", " + this.state.time}
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className="main">
+        <div className="date-time">{date + ", " + tyme}</div>
+      </div>
+    </>
+  );
+};
 
 export default App;
+
+// class component::::
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       time: new Date().toLocaleTimeString(),
+//       date: new Date().toLocaleDateString()
+//     };
+//   }
+
+//   runningTime() {
+//     this.setState({
+//       time: new Date().toLocaleTimeString()
+//     });
+//   }
+
+//   componentDidMount() {
+//     this.timer = setInterval(() => this.runningTime(), 1000);
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timer);
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <div className="date-time">
+//           {this.state.date + ", " + this.state.time}
+//         </div>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
